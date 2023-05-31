bb10b5352705e1dc7769e31a993b62487fc77b3e
Justin Yamberla <justin.yamberla@epn.edu.ec>
2023-05-31 13:37:38 -0500

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -45,13 +45,13 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', {origin: 'top'})
 ScrollReveal().reveal('.home.img, .services-container, .portfolio-box, contact form', {origin: 'bottom'})
-ScrollReveal().reveal('.home.content h1, .about-img', {origin: 'left'})
+ScrollReveal().reveal('.home.content h1, .about-img, .about-text', {origin: 'left'})
 
 /*================ typed js =====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Designer', 'Content Creator'],
-    typeSpeed: 100,
-    backSpeed: 100,
+    strings: ['a Software Engineer', 'a Full Stack Developer', 'an UX/UI Designer'],
+    typeSpeed: 70,
+    backSpeed: 70,
     backDelay: 1000,
     loop: true
 })
